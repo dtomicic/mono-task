0e1f282972a1b27ec9e2f1b20b181808160cb11f
darko.tomicic <dtomicic192@gmail.com>
2023-08-04 20:22:06 +0200

    navigation done

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Makes</a>
+        </li>
+        <li>
+          <a href="/models">Models</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;

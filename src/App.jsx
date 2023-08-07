4ebe816b11628f5ce86629c13e79c64d9fbd8d24
darko.tomicic <dtomicic192@gmail.com>
2023-08-07 20:58:36 +0200

    routes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import Navigation from "./components/Navigation/Navigation";
-import Table from "./components/Table/Table";
 
 function App() {
   return (
-    <>
+    <Router>
       <Navigation />
-      <div className="main-section">
-        <Table />
-      </div>
-    </>
+    </Router>
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import { Route, Routes, Link } from "react-router-dom";
+import Models from "../../pages/Models/Models";
 import "./style.css";
+import Home from "../../pages/Home/Home";
 
 function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <a href="/">Makes</a>
-        </li>
-        <li>
-          <a href="/models">Models</a>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Makes</Link>
+          </li>
+          <li>
+            <Link to="/models">Models</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/models" element={<Models />} />
+      </Routes>
+    </>
   );
 }
 
